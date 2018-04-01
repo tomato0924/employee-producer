0aeca7a029e3a0230ca56f7869fb22d9132c8973
hongkook <hongkook@tomatoLaptop>
2018-04-01 20:34:06 +0900

    Hystrix - fallback

--- src/main/java/com/javainuse/SpringBootHelloWorldApplication.java
+++ src/main/java/com/javainuse/SpringBootHelloWorldApplication.java
@@ -2,9 +2,11 @@ package com.javainuse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableCircuitBreaker			// Hystrix 를 통해 fallback 메소드 구현
 @EnableDiscoveryClient
 
 public class SpringBootHelloWorldApplication {
--- src/main/java/com/javainuse/controllers/TestController.java
+++ src/main/java/com/javainuse/controllers/TestController.java
@@ -5,11 +5,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.javainuse.model.Employee;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class TestController {
 
 	@RequestMapping(value = "/employee", method = RequestMethod.GET)
+	@HystrixCommand(fallbackMethod = "getDataFallBack")		// Hystrix - fallback method 정의
 	public Employee firstPage() {
 
 		Employee emp = new Employee();
@@ -17,8 +19,23 @@ public class TestController {
 		emp.setDesignation("manager");
 		emp.setEmpId("1");
 		emp.setSalary(3000);
+		
+		if(emp.getName().equalsIgnoreCase("emp1"))
+			throw new RuntimeException();
 
 		return emp;
 	}
 
+	public Employee getDataFallBack() {
+		
+		Employee emp = new Employee();
+		emp.setName("fallback-emp1");
+		emp.setDesignation("fallback-manager");
+		emp.setEmpId("fallback-1");
+		emp.setSalary(3000);
+
+		return emp;
+		
+	}
+	
 }
